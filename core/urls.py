828d0a10dc401a931d9b0d2f1d86caea234af074
amalazza <dinaazza8@gmail.com>
2021-05-09 17:24:41 +0700

    url corenya lupa

--- core/urls.py
+++ core/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),          # Django admin route
     path("", include("authentication.urls")), # Auth routes - login / register
     path("", include("app.urls")),             # UI Kits Html files
+    path("layanan/kuesioner/pertanyaan/", include("pertanyaan.urls")), 
 
 ]
